bc0bb2ca03bf0723ca4ff99e219b5ed4625beba2
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-06-30 05:20:08 +0545

    Added an app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// Flutter is Widget over Widget over Widget 
+// And every Widget has arguments inside it
+// When we use an argument we needt to add another widget inside it 
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('App Title'),
+        ),
+      ),
     );
   }
 }
